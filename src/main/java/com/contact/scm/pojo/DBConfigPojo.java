f61c71a3f3565091d4cdf19cfe1f326c68414ca5
Hitesh Sardar <hiteshsardar1998@gmail.com>
2024-12-27 21:19:07 +0530

    upgraded to java 21

---
+++ src/main/java/com/contact/scm/pojo/DBConfigPojo.java
@@ -0,0 +1,11 @@
+package com.contact.scm.pojo;
+
+import lombok.Data;
+
+@Data
+public class DBConfigPojo {
+    private String username;
+    private String password;
+    private String host;
+    private String port;
+}
